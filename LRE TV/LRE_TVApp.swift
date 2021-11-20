c8581c9e6edcab32372bfc8b84dcbb44a9e13ec0
Michael Edlund <medlund@mac.com>
2021-11-20 15:51:16 -0800

    Initial Commit

---
+++ LRE TV/LRE_TVApp.swift
@@ -0,0 +1,17 @@
+//
+//  LRE_TVApp.swift
+//  LRE TV
+//
+//  Created by Michael Edlund on 11/20/21.
+//
+
+import SwiftUI
+
+@main
+struct LRE_TVApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
